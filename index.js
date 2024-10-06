11755070eb74a148c6179bfddc769b5616b07288
Manikanta8887 <swamimanikanta66@gmail.com>
2024-10-06 15:01:58 +0530

    Adding files

---
+++ index.js
@@ -0,0 +1,53 @@
+//  1st Question
+
+function manikanta(a,b){
+    console.log(a+b);
+}
+manikanta(10,20);
+manikanta(20,60);
+
+function add(a,b){
+    return a + b;
+}
+var result=add(20,30);
+var result1=add(50,50);
+console.log(result);
+console.log(result1);
+
+//  2nd Question
+
+function Sub(a,b){
+    return a-b;
+}
+var result=Sub(30,10);
+var result3=Sub(50,10);
+console.log(result);
+console.log(result3);
+
+//  3rd Question
+
+function Multiply(a,b){
+    return b*a;
+}
+var result=Multiply(10,20);
+console.log(result);
+
+
+//   4th Question
+
+function div(a,b){ 
+    if(b==0){
+        return("Cannot divide by zero");
+    }
+    else{
+        return(a/b);
+    }
+}
+var result5=div(100,0);
+var result6=div(100,20);
+console.log(result5);
+console.log(result6);
+
+
+
+ 
